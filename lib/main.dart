ef7f423e0db66621be07b85e3d2ffea1365df617
Алексей Поступинский <154533@mail.ru>
2023-08-09 15:27:20 +0500

    09.08.2023

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,8 @@ import 'package:televerse/televerse.dart';
  const String token = "6622522645:AAEzQznIXLr-gFT3pVT5LK8Zlsuthbr8Mzk";
  final Bot bot = Bot(token);
  Conversation conversation = Conversation(bot);
+
+  
 void main() {
   
   runApp(const MyApp());
@@ -55,46 +57,12 @@ class _MyHomePageState extends State<MyHomePage> {
     )
     );
   }
-
-  MessageHandler handler(String text)
-  {
-    return((ctx) async => await ctx.reply("Got a $text")); 
-  }
-
+  
+  
   _startBot() async
   {
-    bot.onAudio(handler("audio"));
-  bot.onDocument(handler("document"));
-  bot.onPhoto(handler("photo"));
-  bot.onSticker(handler("sticker"));
-  bot.onVideo(handler("video"));
-  bot.onVideoNote(handler("video note"));
-  bot.onVoice(handler("voice"));
-  bot.onContact(handler("contact"));
-  bot.onDice(handler("dice"));
-  bot.onGame(handler("game"));
-  bot.onPollMessage(handler("poll"));
-  bot.onVenue(handler("venue"));
-  bot.onLocation(handler("location"));
-  bot.onLiveLocation(handler("live location"));
-  bot.onNewChatTitle(handler("new chat title"));
-  bot.onNewChatPhoto(handler("new chat photo"));
-  bot.onDeleteChatPhoto(handler("delete chat photo"));
-  bot.onPinnedMessage(handler("pinned message"));
-  bot.onUsrShared(handler("user shared"));
-  bot.onChatShared(handler("chat shared"));
-  bot.whenVideoChatScheduled(handler("video chat scheduled"));
-  bot.whenVideoChatStarted(handler("video chat started"));
-  bot.whenVideoChatEnded(handler("video chat ended"));
-  bot.whenVideoChatParticipantsInvited(
-    handler("video chat participants invited"),
-  );
-  bot.onForumTopicCreated(handler("forum topic created"));
-  bot.onForumTopicEdited(handler("forum topic edited"));
-  bot.onForumTopicClosed(handler("forum topic closed"));
-  bot.onForumTopicReopened(handler("forum topic reopened"));
-  bot.onWebAppData(handler("web app data"));
-  bot.onAnimation(handler("animation"));
+    bot.command("test", (ctx) => ctx.reply( "Hello <b>world</b>",
+      parseMode: ParseMode.html,)); 
     // bot.command("start", (ctx) { String name = ctx.message.from!.firstName;
     //   ctx.reply("Hello $name!");});
     bot.start();
